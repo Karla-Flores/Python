7cb68b458759860fb341972ea0a28f2591736ef0
Karla-Flores <ms.kaflores@gmail.com>
2021-05-04 22:09:53 -0500

    Final result -Successful outout of TXT file

--- PyBank/Main.py
+++ PyBank/Main.py
@@ -71,6 +71,17 @@ print ("Financial Analysis")
 print ("---------------------------------------------------\n")
 print (f"Total Months: {count}")
 print (f"Total: ${total: ,}")
-print (f"Average Change: $ {Avg: ,} ")
+print (f"Average Change: $ {Avg: ,}")
 print (f"Greatest Increase in Profits: {increse_date}  (${increase:,})")
 print (f"Greatest Decrease in Profits: {decrese_date}  (${decrease:,})")
+
+#Export TXT file
+b_file = os.path.join("/Users/karlaflores/Desktop/Git/Python----Challenge/PyBank/Output/budget_data.txt")
+with open(b_file, "w") as outfile:
+    outfile.write("Financial Analysis\n")
+    outfile.write("---------------------------------------------------\n")
+    outfile.write(f"Total Months: {count}\n")
+    outfile.write(f"Total: ${total: ,}\n")
+    outfile.write(f"Average Change: $ {Avg: ,}\n")
+    outfile.write(f"Greatest Increase in Profits: {increse_date}  (${increase:,})\n")
+    outfile.write(f"Greatest Decrease in Profits: {decrese_date}  (${decrease:,})\n")
